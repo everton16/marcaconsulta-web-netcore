655ef74d32c2984209b3d168fa33cd8376f08c86
Everton M Silva <everton16@gmail.com>
2023-07-31 01:12:59 -0300

    Alterações Gerais

---
+++ marcaconsulta-web-netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace marcaconsulta_web_netcore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
